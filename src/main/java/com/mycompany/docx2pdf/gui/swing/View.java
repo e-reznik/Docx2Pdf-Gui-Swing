2d65000f2c68ac9fc03ccf3759ab339cab2a8931
Evgenij Reznik <evgenij.reznik@gmx.de>
2020-11-20 11:00:18 +0100

    Connected with Converter

--- src/main/java/com/mycompany/docx2pdf/gui/swing/View.java
+++ src/main/java/com/mycompany/docx2pdf/gui/swing/View.java
@@ -1,27 +1,34 @@
 package com.mycompany.docx2pdf.gui.swing;
 
+import docx2pdf.Converter;
 import java.awt.BorderLayout;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.io.FileNotFoundException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.JButton;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.filechooser.FileNameExtensionFilter;
 
 public class View extends JFrame {
+
+    JTextField tfDocx = new JTextField();
+    JButton bDocx = new JButton("Browse...");
+
+    JTextField tfPdf = new JTextField();
+    JButton bPdf = new JButton("Browse...");
+
+    JTextField tfFonts = new JTextField();
+    JButton bFonts = new JButton("Browse...");
 
     public View() {
         JPanel panel = new JPanel();
         panel.setLayout(new GridLayout(3, 3));
-
-        JTextField tfDocx = new JTextField();
-        JButton bDocx = new JButton("Browse...");
-
-        JTextField tfPdf = new JTextField();
-        JButton bPdf = new JButton("Browse...");
-
-        JTextField tfFonts = new JTextField();
-        JButton bFonts = new JButton("Browse...");
 
         panel.add(new JLabel("DOCX:"));
         panel.add(tfDocx);
@@ -42,8 +49,64 @@ public class View extends JFrame {
         JButton bGenerate = new JButton("Generate");
         this.add(bGenerate, BorderLayout.SOUTH);
 
-        this.setSize(400, 500);
+        //
+        bDocx.addActionListener((ActionEvent e) -> {
+            chooseDocx();
+        });
+        bPdf.addActionListener((ActionEvent e) -> {
+            choosePdf();
+        });
+        bFonts.addActionListener((ActionEvent e) -> {
+            chooseFonts();
+        });
+        bGenerate.addActionListener((ActionEvent e) -> {
+            generate();
+        });
+
+        this.setSize(700, 300);
         this.setVisible(true);
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
+
+    private void chooseDocx() {
+        JFileChooser chooser = new JFileChooser();
+        FileNameExtensionFilter filter = new FileNameExtensionFilter(
+                "Docx files", "docx");
+        chooser.setFileFilter(filter);
+        int returnVal = chooser.showOpenDialog(this);
+        if (returnVal == JFileChooser.APPROVE_OPTION) {
+            tfDocx.setText(chooser.getSelectedFile().getAbsolutePath());
+        }
+    }
+
+    private void choosePdf() {
+        JFileChooser chooser = new JFileChooser();
+        chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+        int returnVal = chooser.showOpenDialog(this);
+        if (returnVal == JFileChooser.APPROVE_OPTION) {
+            tfPdf.setText(chooser.getSelectedFile().getAbsolutePath());
+        }
+    }
+
+    private void chooseFonts() {
+        JFileChooser chooser = new JFileChooser();
+        chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+        int returnVal = chooser.showOpenDialog(this);
+        if (returnVal == JFileChooser.APPROVE_OPTION) {
+            tfFonts.setText(chooser.getSelectedFile().getAbsolutePath());
+        }
+    }
+
+    private void generate() {
+        try {
+            String in = tfDocx.getText();
+            String out = tfPdf.getText() + "/out.pdf";
+            String fontsFolder = tfFonts.getText();
+
+            Converter c = new Converter(in, out, fontsFolder);
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(View.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
 }
