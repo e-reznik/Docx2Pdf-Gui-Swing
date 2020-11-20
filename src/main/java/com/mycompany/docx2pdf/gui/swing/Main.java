009e8bf0fd3e53cc66a97a85379031df936ac8b0
Evgenij Reznik <evgenij.reznik@gmx.de>
2020-11-20 10:34:07 +0100

    Added elements to view

--- src/main/java/com/mycompany/docx2pdf/gui/swing/Main.java
+++ src/main/java/com/mycompany/docx2pdf/gui/swing/Main.java
@@ -2,4 +2,9 @@ package com.mycompany.docx2pdf.gui.swing;
 
 public class Main {
 
+    public static void main(String[] args) {
+        View view = new View();
+
+    }
+
 }
--- src/main/java/com/mycompany/docx2pdf/gui/swing/View.java
+++ src/main/java/com/mycompany/docx2pdf/gui/swing/View.java
@@ -1,5 +1,49 @@
 package com.mycompany.docx2pdf.gui.swing;
 
-public class View {
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
+public class View extends JFrame {
+
+    public View() {
+        JPanel panel = new JPanel();
+        panel.setLayout(new GridLayout(3, 3));
+
+        JTextField tfDocx = new JTextField();
+        JButton bDocx = new JButton("Browse...");
+
+        JTextField tfPdf = new JTextField();
+        JButton bPdf = new JButton("Browse...");
+
+        JTextField tfFonts = new JTextField();
+        JButton bFonts = new JButton("Browse...");
+
+        panel.add(new JLabel("DOCX:"));
+        panel.add(tfDocx);
+        panel.add(bDocx);
+
+        panel.add(new JLabel("PDF:"));
+        panel.add(tfPdf);
+        panel.add(bPdf);
+
+        panel.add(new JLabel("Fonts folder:"));
+        panel.add(tfFonts);
+        panel.add(bFonts);
+
+        this.setLayout(new BorderLayout());
+        this.add(panel, BorderLayout.CENTER);
+
+        //
+        JButton bGenerate = new JButton("Generate");
+        this.add(bGenerate, BorderLayout.SOUTH);
+
+        this.setSize(400, 500);
+        this.setVisible(true);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
 }
